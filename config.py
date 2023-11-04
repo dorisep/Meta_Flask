1a644ad14b899e406f31fdafbbd8d9f1a28e4be1
dorisep <ed.doris@gmail.com>
2023-11-04 14:13:48 -0500

    added config file

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
         or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+###
+#bash commands for setting env vars
+#
+#export SECRET_KEY="your secret key"
+#
+#export DATABASE_URI="postgresql://username:password@host:port/database_name"
+#
+###
